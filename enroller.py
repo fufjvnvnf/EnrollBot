785899325ec4832176d74b6d517a0c79af093b4a
fufjvnvnf <fufjvnvnf@gmail.com>
2017-01-08 01:22:45 +0800

    working on step 1

--- enroller.py
+++ enroller.py
@@ -4,11 +4,13 @@ from bs4 import BeautifulSoup
 
 # Global Session object
 s = requests.session()
+g_netid = ""
+g_pwd = ""
 
-def login():
+def login(n, p):
 
-    netid = input('NetID: ')
-    pwd = input('Password: ')
+    netid = n
+    pwd = p
     
     s.headers.update({'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.95 Safari/537.36'})
     
@@ -28,10 +30,12 @@ def login():
     try:
         cookie = s.cookies['cuwlrelogin']
         print ("Success")
+        g_netid = n
+        g_pwd = p
 
     except KeyError:
         print ("Login fails.")
-        login()
+        login(input('NetID: '), input('Password: '))
     
     #c0ntinue
     url3 = BeautifulSoup(r2.content,"lxml").form.get('action')
@@ -44,7 +48,42 @@ def login():
         'Referer': url2,
         'Host': 'css.adminapps.cornell.edu'}
     r3 = s.post(url3, data=data3, cookies = ccookies, headers = headers)
-    print (r3.content)
+    
+def relogin():
+    login(g_netid, g_pwd)
+    
+# used to find hidden values for form data
+def findHidden(strs, content):
+    data = {}
+    for arg in strs:
+        try:
+            data[arg] = BeautifulSoup(content,"lxml").find('input', {'id': arg}).get('value')
+        except:
+            pass
+    return data
+    
 
-login()
+if __name__ == '__main__':
+    login(input('NetID: '), input('Password: '))
+    #click enroll on student center main page
+    url4 = 'https://css.adminapps.cornell.edu/psc/cuselfservice/EMPLOYEE/HRMS/c/SA_LEARNER_SERVICES.SSR_SSENRL_CART.GBL?Page=SSR_SSENRL_CART&Action=A&ExactKeys=Y&TargetFrameName=None'
+    r4 =  s.get(url4)
+    
+    # proceeding to step 2
+    url5 = BeautifulSoup(r4.content,"lxml").find('form', {'name': 'win0'}).get('action')
+    step2inputs = ['ICElementNum','ICStateNum','ICAction','ICXPos','ICYPos','ResponsetoDiffFrame',
+    'TargetFrameName','FacetPath','ICFocus','ICSaveWarningFilter','ICChanged','ICResubmit','ICSID','ICActionPrompt', 
+    'ICFind','ICAddCount','ICAPPCLSDATA']
+    step1data =  findHidden(step2inputs, r4.content)
+    step1data['DERIVED_SSTSNAV_SSTS_MAIN_GOTO$7$'] = '9999'
+    step1data['DERIVED_REGFRM1_CLASS_NBR'] = ''
+    step1data['DERIVED_REGFRM1_SSR_CLS_SRCH_TYPE$249$'] = '06'
+    step1data['DERIVED_SSTSNAV_SSTS_MAIN_GOTO$8$'] = '9999'
+    step1headers = {
+        'Origin': 'https://css.adminapps.cornell.edu',
+        'Referer': url4,
+        'Host': 'css.adminapps.cornell.edu'}
+    r5 = s.post(url5, data = step1data, headers = step1headers)
+    print (r5.content)
+    
 
